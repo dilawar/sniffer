6ad3bdd5b86a5d7ccfff0a05c04ad52d6765e799
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-11-18 13:54:57 +0530

    Added a line for text support

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,4 @@
-import sqlite3 as sql 
+import sqlite3 as sql
 import os, sys
 import difflib
 
@@ -8,17 +8,19 @@ def formatText(txt, lang) :
     obj = vhdl.VHDL()
     return obj.fix_text(txt)
   if lang == "verilog" :
-    import lang_verilog as verilog 
+    import lang_verilog as verilog
     obj = verilog.Verilog()
     return obj.fix_text(txt)
   if lang == "pdf" :
-    import lang_pdf as pdf 
+    import lang_pdf as pdf
     obj = pdf.Pdf()
     return obj.fix_text(txt)
+  if lang == "text":
+      return txt
   else :
     print("[W] This language is not supported. Assuming text ...")
     return txt
-  
+
 def compareAndReturnResult(textA, textB, algorithm="subsequence" ) :
   if algorithm == "subsequence" :
     try :
@@ -38,22 +40,22 @@ def compareAndReturnResult(textA, textB, algorithm="subsequence" ) :
 
     lR = float(len(wordsA)) / len(wordsB)
     if lR > 1.0 :
-      lR = 1/lR 
+      lR = 1/lR
     if lR < 0.2 :
       return "Files differ in sizes", lR
 
-    # check intersection of keywords 
+    # check intersection of keywords
     setA = set(wordsA)
     setB = set(wordsB)
-      
-    intersectionSize =  float(len(setA.intersection(setB))) / min(len(setA), len(setB)) 
+
+    intersectionSize =  float(len(setA.intersection(setB))) / min(len(setA), len(setB))
     if intersectionSize < 0.35 :
-      return "Small keyword intersection", intersectionSize 
+      return "Small keyword intersection", intersectionSize
 
     # if intersection of these two set is very small then there is little
-    # maching in these two files. 
+    # maching in these two files.
     s = difflib.SequenceMatcher(None, textA, textB)
-    return "difflib", s.ratio() 
+    return "difflib", s.ratio()
 
   else :
     print("[E] Algorithm not specified")
@@ -70,4 +72,4 @@ def commonPrefix(string1, string2) :
     else :
       done = True
     i += 1
-  return prefix 
+  return prefix
--- sniffer.py
+++ sniffer.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python2.7
 import re
 import os, sys
-import argparse 
-import database 
-import compare 
+import argparse
+import database
+import compare
 
 if __name__ == "__main__" :
 
   parser = argparse.ArgumentParser(description="This is code sniffer.")
   parser.add_argument('--config', metavar='filepath'
-      , default="~/.snifferrc"
+      , default=os.environ['HOME']+"/.snifferrc"
       , help = "Path of configuration file."
       )
   args = parser.parse_args()
-  configFile = args.config 
+  configFile = args.config
 
   # check if file exists .
   if not os.path.exists(configFile) :
     print("[E] File {0} does not exists".format(configFile))
     sys.exit(0)
-  
+
   # else parse it.
   import ConfigParser as cfg
   config = cfg.ConfigParser()
   config.read(configFile)
-  db = database.buildListingDb(config) 
+  db = database.buildListingDb(config)
   if config.get("source", "compare") == "true" :
     compare.compare(config, db)
     database.dump(config, db)
