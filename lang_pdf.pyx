f5a243feb688c574e100148cb9e2508c354a6238
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-11-19 00:25:48 +0530

    pdfparsing is now easy. But it fails with error

--- lang_pdf.pyx
+++ lang_pdf.pyx
@@ -1,27 +1,10 @@
 from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
 from cStringIO import StringIO
 
 class Pdf :
   def __init__(self) : pass
-
-  def parse_lt_objs (lt_objs, page_number, images_folder, text=[]):
-    """Iterate through the list of LT* objects and capture the text or image data contained in each"""
-    text_content = []
-
-    page_text = {} # k=(x0, x1) of the bbox, v=list of text strings within that bbox width (physical column)
-    for lt_obj in lt_objs:
-        if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-            # text, so arrange is logically based on its column width
-            page_text = update_page_text_hash(page_text, lt_obj)
-    for k, v in sorted([(key,value) for (key,value) in page_text.items()]):
-        # sort the page_text hash by the keys (x0,x1 values of the bbox),
-        # which produces a top-down, left-to-right sequence of related columns
-        text_content.append(''.join(v))
-
-    return '\n'.join(text_content)
-
 
   def fix_text_tem(self, path, lang_type):
     rsrcmgr = PDFResourceManager()
