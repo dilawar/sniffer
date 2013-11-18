ebb92d949ea74861a815d6c43ca42ae420278b46
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-11-18 23:26:51 +0530

    This seems to works

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("sniffer",
-                             ["compare.pyx"
-                              , "lang_ctype.pyx"
-                              , "lang_vhdl.pyx"
-                              , "lang_verilog.pyx"
-                              , "lang_pdf.pyx"
-                              , "lcs.pyx"
-                              , "save_email.pyx"
-                              , "algorithm.pyx"
-                              ])]
+    ext_modules = cythonize("./*.pyx")
 )
